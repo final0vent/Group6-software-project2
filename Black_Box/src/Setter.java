7040f1beed0dcac9045c79238a9a73b4a462d62d
final0vent <48002040+final0vent@users.noreply.github.com>
2024-02-13 10:28:29 +0000

    Add files via upload

---
+++ Black_Box/src/Setter.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Setter {
+    public void rand_place() {
+        //Create instance of random class
+        Random random = new Random();
+
+        //Generate random coordinate number array in range 0~4
+        int[][] coordinate_rand = new int[6][3];
+        for(int i = 0 ; i < 6 ; i++){
+            for(int j = 0 ; j < 3 ; j++)
+            {
+                coordinate_rand[i][j] = random.nextInt(5);
+            }
+        }
+
+        //Print the random coordinate number array
+        System.out.println(Arrays.deepToString(coordinate_rand));
+    }
+
+    public void manu_place(){
+        int[][] coordinate_manu = new int[6][3];
+
+        Scanner scanner = new Scanner(System.in);
+        for(int i = 0 ; i < 6 ; i++){
+            for(int j = 0 ; j < 3 ; j++)
+            {
+                coordinate_manu[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println(Arrays.deepToString(coordinate_manu));
+    }
+
+    public static void main(String[] args) {
+        Setter setter = new Setter();
+        setter.rand_place();
+    }
+}
