c56100585e9047826e5c7b230b014435ae35a53e
TrinityCollege6 <157810633+TrinityCollege6@users.noreply.github.com>
2024-02-19 17:31:57 +0000

    Add files via upload

--- Black_Box/src/MainWindow.java
+++ Black_Box/src/MainWindow.java
@@ -11,7 +11,7 @@ public class MainWindow {
         frame.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-                int choice = JOptionPane.showConfirmDialog(frame, "Are you sure you want to close?", "Confirm Close", JOptionPane.YES_NO_OPTION);
+                int choice = JOptionPane.showConfirmDialog(frame, "Are you sure you want to close Black Box?", "Confirm Close", JOptionPane.YES_NO_OPTION);
                 if (choice == JOptionPane.YES_OPTION) {
                     frame.dispose(); // Close the window
                 }
