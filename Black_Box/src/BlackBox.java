2661ff64fd7a43a647a602dd3e657c53e7b7327b
Guanqiao Han <han254319205@gmail.com>
2024-02-23 16:54:11 +0000

    For spirit 1

--- Black_Box/src/BlackBox.java
+++ Black_Box/src/BlackBox.java
@@ -1,7 +1,6 @@
 import javax.swing.JFrame;
 import javax.swing.*;
 import java.awt.event.*;
-import javax.swing.JComboBox;
 import javax.swing.JButton;
 
 import java.awt.Dimension;
@@ -9,15 +8,11 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.FlowLayout;
 import java.awt.BorderLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseListener;
-
 
 public class BlackBox extends JPanel implements BlackBoxConfig
 {
     public Graphics g;
     //public int[][][] coordanites = new int [8][8][8];
-    public int[][] isAvail = new int [15][15];
 
     public static void main(String args[])
     {
@@ -29,7 +24,7 @@ public class BlackBox extends JPanel implements BlackBoxConfig
     {
         JFrame jf = new JFrame();
         jf.setTitle("Black Box");
-        jf.setSize(800,650);
+        jf.setSize(800,800);
         jf.setLocationRelativeTo(null);
         jf.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
@@ -48,14 +43,15 @@ public class BlackBox extends JPanel implements BlackBoxConfig
         Dimension dim1 = new Dimension(150, 0);
         Dimension dim3 = new Dimension(550, 0);
         Dimension dim2 = new Dimension(140,40);
+        Dimension DIM = new Dimension(600,600);
 
-        this.setPreferredSize(dim3);
+        this.setPreferredSize(DIM);                             //This for main part
         this.setBackground(Color.LIGHT_GRAY);
 
         jf.add(this,BorderLayout.CENTER);
 
         JPanel jp = new JPanel();
-        jp.setPreferredSize(dim1);
+        jp.setPreferredSize(dim2);                              //This for button
         jp.setBackground(Color.WHITE);
         jf.add(jp,BorderLayout.EAST);
         jp.setLayout(new FlowLayout());
@@ -73,57 +69,41 @@ public class BlackBox extends JPanel implements BlackBoxConfig
         jf.setVisible(true);
     }
 
+    public void drawHexagon(Graphics g,double x,double y){
+        for(int i = 0 ; i < 6 ; i++){
+            g.drawLine((int)(x+r*Math.sin(a*i)),(int)(y+r*Math.cos(a*i)),(int)(x+r*Math.sin(a*(i+1))),(int)(y+r*Math.cos(a*(i+1))));
+        }
+    }
 
     public void paint(Graphics g)
     {
+        double up_x,down_x;
+        double up_y,down_y;
+        double temp=mid_x;
+
         super.paint(g);
-
         g.setColor(Color.BLACK);
-        for(int i = 0; i < row; i ++)
-        {
-            g.drawLine(x, y + size * i, x + size * (column - 1), y + size * i);
-        }
 
-        for(int j = 0; j < column; j ++)
-        {
-            g.drawLine(x + size * j, y, x + size * j, y + size * (row - 1));
-        }
-    }
-
-}
-
-
-/*
-public class MainWindow {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Display Window");
-        frame.setSize(800, 650);
-        frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-
-        frame.addWindowListener(new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent e) {
-                int choice = JOptionPane.showConfirmDialog(frame, "Are you sure you want to close Black Box?", "Confirm Close", JOptionPane.YES_NO_OPTION);
-                if (choice == JOptionPane.YES_OPTION) {
-                    frame.dispose();
-                }
+        for(int j = 1 ; j <= 4 ; j++){
+            up_x = mid_x + j*length;
+            up_y = y - ((2*j)*length)*Math.sin(a);
+            for(int i = 0 ; i<9-j ; i++){
+                drawHexagon(g,up_x,up_y);
+                up_x += 2*length;
             }
-        });
-
-        frame.setLayout(new BorderLayout());
-
-        Dimension dim1 = new Dimension(150, 0);
-        Dimension dim3 = new Dimension(550, 0);
-        Dimension dim2 = new Dimension(140,40);
-
-        frame.setPreferredSize(dim3);
-        frame.setBackground(Color.LIGHT_GRAY);
-
-
-
-
-        frame.setVisible(true);
+        }
+        for(int i = 0 ; i<9 ; i++){
+            drawHexagon(g,temp,y);
+            temp += 2*(r*Math.sin(a));
+        }
+        for(int j = 1 ; j <= 4 ; j++){
+            down_x = mid_x + j*length;
+            down_y = y + ((2*j)*length)*Math.sin(a);
+            for(int i = 0 ; i<9-j ; i++){
+                drawHexagon(g,down_x,down_y);
+                down_x += 2*length;
+            }
+        }
     }
+
 }
-*/
--- Black_Box/src/BlackBoxConfig.java
+++ Black_Box/src/BlackBoxConfig.java
@@ -1,4 +1,9 @@
 public interface BlackBoxConfig
 {
-    int x = 20, y = 20, size = 40, row = 15, column = 15;
+    int size = 40, row = 15, column = 15;
+    int r = 30;
+    double x = 300, y = 300;
+    double a = 2*Math.PI / 6;
+    double length = r*Math.sin(a);
+    double mid_x = 300 - 8*r;
 }
