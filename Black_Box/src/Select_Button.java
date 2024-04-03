551874188e1847920f772825359caef657d83d6a
Guanqiao Han <han254319205@gmail.com>
2024-04-03 21:21:14 +0100

    spirit 3 box UI update

--- Black_Box/src/Select_Button.java
+++ Black_Box/src/Select_Button.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class Select_Button implements BlackBoxConfig {
     public int[][] select_button(){
@@ -9,8 +11,8 @@ public class Select_Button implements BlackBoxConfig {
         int loop4_p = 5,loop4_q=-1;
         int loop5_p = 1,loop5_s = -5;
         int loop6_q = 5,loop6_s = -1;
-        int[][] ButtonArr = new int[55][2];
 
+        int[][] ButtonArr = new int[55][6];
 
         for(int i = 1 ; i<= 9 ; i++){
             if(i % 2 != 0){
@@ -23,6 +25,9 @@ public class Select_Button implements BlackBoxConfig {
                 ButtonArr[i][0] = (int)(bbt.getX(loop1_p,loop1_q,4) - pl_detX);
                 ButtonArr[i][1] = (int)(bbt.getY(loop1_p,loop1_q,4) - pl_detY);
             }
+            ButtonArr[i][2] = loop1_p;
+            ButtonArr[i][3] = loop1_q;
+            ButtonArr[i][4] = 4;
         }
         for(int i = 10 ; i<= 18 ; i++){
             if(i % 2 == 0){
@@ -35,6 +40,9 @@ public class Select_Button implements BlackBoxConfig {
                 ButtonArr[i][0] = (int)(bbt.getX(loop2_p,-4,loop2_s) - sg_detX);
                 ButtonArr[i][1] = (int)(bbt.getY(loop2_p,-4,loop2_s) + sg_detY);
             }
+            ButtonArr[i][2] = loop2_p;
+            ButtonArr[i][3] = -4;
+            ButtonArr[i][4] = loop2_s;
         }
         for(int i = 19 ; i<= 27 ; i++){
             if(i % 2 != 0){
@@ -47,6 +55,9 @@ public class Select_Button implements BlackBoxConfig {
                 ButtonArr[i][0] = (int)(bbt.getX(4,loop3_q,loop3_s) + sg_detX);
                 ButtonArr[i][1] = (int)(bbt.getY(4,loop3_q,loop3_s) + sg_detY);
             }
+            ButtonArr[i][2] = 4;
+            ButtonArr[i][3] = loop3_q;
+            ButtonArr[i][4] = loop3_s;
         }
         for(int i = 28 ; i<= 36 ; i++){
             if(i % 2 == 0){
@@ -59,6 +70,9 @@ public class Select_Button implements BlackBoxConfig {
                 ButtonArr[i][0] = (int)(bbt.getX(loop4_p,loop4_q,-4) + pl_detX);
                 ButtonArr[i][1] = (int)(bbt.getY(loop4_p,loop4_q,-4) + pl_detY);
             }
+            ButtonArr[i][2] = loop4_p;
+            ButtonArr[i][3] = loop4_q;
+            ButtonArr[i][4] = -4;
         }
         for(int i = 37 ; i<= 45 ; i++){
             if(i % 2 == 0){
@@ -71,6 +85,9 @@ public class Select_Button implements BlackBoxConfig {
                 ButtonArr[i][0] = (int)(bbt.getX(loop5_p,4,loop5_s) + pl_detX);
                 ButtonArr[i][1] = (int)(bbt.getY(loop5_p,4,loop5_s) + pl_detY);
             }
+            ButtonArr[i][2] = loop5_p;
+            ButtonArr[i][3] = 4;
+            ButtonArr[i][4] = loop5_s;
         }
         for(int i = 46 ; i<= 54 ; i++){
             if(i % 2 == 0){
@@ -83,7 +100,77 @@ public class Select_Button implements BlackBoxConfig {
                 ButtonArr[i][0] = (int)(bbt.getX(-4,loop6_q,loop6_s) - sg_detX);
                 ButtonArr[i][1] = (int)(bbt.getY(-4,loop6_q,loop6_s) - sg_detY);
             }
+            ButtonArr[i][2] = -4;
+            ButtonArr[i][3] = loop6_q;
+            ButtonArr[i][4] = loop6_s;
         }
         return ButtonArr;
+    }
+
+    public String[] entryDirecion()
+    {
+        String[] buttonDirection = new String[56];
+
+        buttonDirection[1] = "SE";
+        buttonDirection[2] = "E";
+        buttonDirection[3] = "SE";
+        buttonDirection[4] = "E";
+        buttonDirection[5] = "SE";
+        buttonDirection[6] = "E";
+        buttonDirection[7] = "SE";
+        buttonDirection[8] = "E";
+        buttonDirection[9] = "SE";
+
+        buttonDirection[10] = "E";
+        buttonDirection[11] = "NE";
+        buttonDirection[12] = "E";
+        buttonDirection[13] = "NE";
+        buttonDirection[14] = "E";
+        buttonDirection[15] = "NE";
+        buttonDirection[16] = "E";
+        buttonDirection[17] = "NE";
+        buttonDirection[18] = "E";
+
+        buttonDirection[19] = "NE";
+        buttonDirection[20] = "NW";
+        buttonDirection[21] = "NE";
+        buttonDirection[22] = "NW";
+        buttonDirection[23] = "NE";
+        buttonDirection[24] = "NW";
+        buttonDirection[25] = "NE";
+        buttonDirection[26] = "NW";
+        buttonDirection[27] = "NE";
+
+        buttonDirection[28] = "NW";
+        buttonDirection[29] = "W";
+        buttonDirection[30] = "NW";
+        buttonDirection[31] = "W";
+        buttonDirection[32] = "NW";
+        buttonDirection[33] = "W";
+        buttonDirection[34] = "NW";
+        buttonDirection[35] = "W";
+        buttonDirection[36] = "NW";
+
+        buttonDirection[37] = "W";
+        buttonDirection[38] = "SW";
+        buttonDirection[39] = "W";
+        buttonDirection[40] = "SW";
+        buttonDirection[41] = "W";
+        buttonDirection[42] = "SW";
+        buttonDirection[43] = "W";
+        buttonDirection[44] = "SW";
+        buttonDirection[45] = "W";
+
+        buttonDirection[46] = "SW";
+        buttonDirection[47] = "SE";
+        buttonDirection[48] = "SW";
+        buttonDirection[49] = "SE";
+        buttonDirection[50] = "SW";
+        buttonDirection[51] = "SE";
+        buttonDirection[52] = "SW";
+        buttonDirection[53] = "SE";
+        buttonDirection[54] = "SW";
+
+        return buttonDirection;
     }
 }
