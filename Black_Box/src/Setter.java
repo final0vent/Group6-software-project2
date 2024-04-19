0c4f777dd9a0818624ad9d9d9bb53408feaafbbe
final0vent <48002040+final0vent@users.noreply.github.com>
2024-04-19 19:28:46 +0100

    Update Setter.java
    S4

--- Black_Box/src/Setter.java
+++ Black_Box/src/Setter.java
@@ -3,21 +3,29 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Setter {
-    public void rand_place() {
+    public int[][] rand_place() {
         //Create instance of random class
         Random random = new Random();
 
         //Generate random coordinate number array in range 0~4
         int[][] coordinate_rand = new int[6][3];
         for(int i = 0 ; i < 6 ; i++){
-            for(int j = 0 ; j < 3 ; j++)
+            for(int j = 0 ; j < 2 ; j++)
             {
-                coordinate_rand[i][j] = random.nextInt(5);
+                coordinate_rand[i][j] = random.nextInt(9)-4;
             }
+            if(coordinate_rand[i][0] + coordinate_rand[i][1] >= -4 && coordinate_rand[i][0] + coordinate_rand[i][1] <=4){
+                coordinate_rand[i][2] = -coordinate_rand[i][0] - coordinate_rand[i][1];
+            }
+            else {
+                i--;
+            }
+
         }
 
         //Print the random coordinate number array
-        System.out.println(Arrays.deepToString(coordinate_rand));
+        //System.out.println(Arrays.deepToString(coordinate_rand));
+        return coordinate_rand;
     }
     public static void main(String[] args) {
         Setter setter = new Setter();
